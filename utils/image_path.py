a26f0f0343403fc1e492e8359bd01ef9590937b1
Your Name <you@example.com>
2024-02-05 19:40:45 +0600

    AKniet

--- utils/image_path.py
+++ utils/image_path.py
@@ -3,6 +3,5 @@ def upload_avatar_for_user(instance, filename):
 
 
 def upload_products(instance, filename):
-    # Проверка и фильтрация данных
-    safe_title = instance.product.title.replace('/', '')  # Или любые другие символы, которые не должны быть в пути
+    safe_title = instance.product.title.replace('/', '')
     return f"products/{safe_title}/{filename}"
